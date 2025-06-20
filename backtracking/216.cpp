d94ce81e2d7aa8e25a0157e2c84ddb8140396a1d
Yannis Zang <xiaoyanzang24@hotmail.com>
2025-06-20 14:11:19 -0400

    backtracking

---
+++ backtracking/216.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <string>
+
+// 216. Combination Sum III
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<vector<int>> res;
+
+        vector<int> ans;
+
+        if (n)
+            backtracking(k, n, 1, 0, res, ans);
+        
+        return res;
+
+    }
+
+    void backtracking(const int& k, const int& n, int idx, int sum, vector<vector<int>>& res, vector<int> ans) {
+        if (sum > n) return;
+        if (ans.size() == k) {
+            // sum += idx;
+            if (sum == n) 
+                res.push_back(ans);
+            return;
+        }
+        for (int i = idx; i < 10; ++i) {
+            ans.push_back(i);
+            backtracking(k, n, i+1, sum + i, res, ans);
+            ans.pop_back();
+        }
+    }
+};
